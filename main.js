9499e8407c5edcc070022cfdfa7a36859116b1fa
Talon LeFevre <113273327+talonlefevre@users.noreply.github.com>
2023-01-01 21:11:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function goToContact() {
+    window.location.href = "contact.html";
+  }
+  
+  function goToProjects() {
+    window.location.href = "projects.html";
+  }
+  function goToHome() {
+    window.location.href = "index.html";
+  }
